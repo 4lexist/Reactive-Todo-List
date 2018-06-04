3405085450db22ffe5cce2bf2f7a8f6708117c4c
Alexis Toromanoff <alexistoromanoff@gmail.com>
2018-06-04 14:32:46 +0200

    Finished twitter user suggestions like component

--- src/components/whoToFollow.component.js
+++ src/components/whoToFollow.component.js
@@ -15,14 +15,11 @@ export default class WhoToFollow extends React.Component {
             clicksOnXStream: '',
         };
         this.refCloseButtons = [];
+        this.NB_SUGGESTIONS = 3;
     }
 
     componentDidMount() {
         const globalRefreshClickStream = Observable.fromEvent(this.refRefreshButton, 'click');
-        const closeClickStreams = [];
-        this.refCloseButtons.map((refButton)=>{
-            closeClickStreams.push(Observable.fromEvent(refButton, 'click'));
-        });
 
         const requestStream = globalRefreshClickStream
             .startWith('simulate a first click to show first batch of users')
@@ -53,44 +50,45 @@ export default class WhoToFollow extends React.Component {
                     console.log(error)
                 })
         });
-        const userSuggestionStreams = [];
-        responseStream.map((responsePromise)=>{
 
-        })
-        this.closeClickStreams.map((clickStream) => {
-            clickStream.subscribe((click)=>{
+        const closeClickStreams = [];
+        this.refCloseButtons.map((refButton) => {
+            closeClickStreams.push(Observable.fromEvent(refButton, 'click'));
+        });
 
+        closeClickStreams.map((clickStream, index) => {
+            const userSuggestionStream = clickStream.combineLatest(responseStream, () => {
+                return this.state.usersArray[Math.floor(Math.random()*(this.state.usersArray.length - this.NB_SUGGESTIONS) + this.NB_SUGGESTIONS)];
+            });
+
+            userSuggestionStream.subscribe((user) => {
+                const newUsersArray = this.state.usersArray;
+                newUsersArray[index] = user;
+
+                this.setState({usersArray: newUsersArray});
             })
         })
 
-        userSuggestionStreams.map((userSuggestion) => {
-
-        })
-    }
-
-    singleRefresh(index) {
-        console.log(index);
-    }
-
-    globalRefresh() {
-        console.log("global");
     }
 
     renderSuggestions(nbSuggestions) {
-        this.refCloseButtons = [];
+        if(this.refCloseButtons.length === 0) {
+            // Need to populate the array on first time because can't map on undefined pointers
+            this.refCloseButtons = [...Array(nbSuggestions)].map((...args) => { return args })
+        }
+
         return (
             <ul className="suggestions">
                 {
-                    this.state.usersArray.map((user, index)=>{
-                        if(index < nbSuggestions){
-                            return (
-                                <li className="suggestion" key={`suggestion-${index}`}>
-                                    <img src={user.avatar}/>
-                                    <p className="username">{user.name}</p>
-                                    <p className="close" ref={(ref) => this.refCloseButtons.push(ref)}> ✕</p>
-                                </li>
-                            )
-                        }
+                    this.refCloseButtons.map((button, index)=>{
+                        return (
+                            <li className="suggestion" key={`suggestion-${index}`}>
+                                <img src={this.state.usersArray[index] ? this.state.usersArray[index].avatar : ''}/>
+                                <p className="username">{this.state.usersArray[index] ? this.state.usersArray[index].name : 'placeholder'}</p>
+                                <p className="close" ref={(ref) => this.refCloseButtons[index] = ref}> ✕</p>
+                            </li>
+                        )
+
                     })
                 }
             </ul>
@@ -103,7 +101,7 @@ export default class WhoToFollow extends React.Component {
                 <div className="header">
                     <h2>Who to follow</h2><a href="#" className="refresh" ref={(ref) => this.refRefreshButton = ref}>Refresh</a>
                 </div>
-                {this.renderSuggestions(3)}
+                {this.renderSuggestions(this.NB_SUGGESTIONS)}
             </div>
         )
     }
